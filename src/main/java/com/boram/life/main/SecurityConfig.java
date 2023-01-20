386105761fd97d14900e0575277f67a096c1f760
tok1224@naver.com <tok12225@gmail.com>
2023-01-20 13:21:30 +0900

    기본 setting 중

---
+++ src/main/java/com/boram/life/main/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.boram.life.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain loginFilter(HttpSecurity http) throws Exception{
+        return http.csrf().disable()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .and()
+                .build();
+
+    }
+}
