15f66480a43b76ee06e04bfe68af69a529fe98a5
boseongzzzzz <boseongzzzzz@gmail.com>
2023-12-15 00:23:56 +0900

    이전 프로젝트 가져오기

--- src/main/java/com/boram/life/member/repository/MemberRepository.java
+++ src/main/java/com/boram/life/member/repository/MemberRepository.java
@@ -1,12 +1,6 @@
 package com.boram.life.member.repository;
 
 import com.boram.life.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
